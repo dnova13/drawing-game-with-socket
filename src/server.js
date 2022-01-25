275aa8624436c027caad9a28c12289ab22cebda1
dnova13 <dnova13@gmail.com>
2022-01-25 14:00:46 +0900

    2.1. socket connect part 1

--- src/server.js
+++ src/server.js
@@ -1,12 +1,13 @@
 import { join } from "path";
 import express from "express";
 import socketIO from "socket.io";
+import logger from "morgan";
 
 const PORT = 4000;
-const app = express()
-
+const app = express();
 app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
+app.use(logger("dev"));
 app.use(express.static(join(__dirname, "static")));
 app.get("/", (req, res) => res.render("home"));
 
@@ -14,5 +15,14 @@ const handleListening = () =>
     console.log(`✅ Server running: http://localhost:${PORT}`);
 
 const server = app.listen(PORT, handleListening);
-const io = socketIO(server)
 
+const io = new socketIO.Server(server);
+
+let sockets = [];
+
+// 소켓 연결. 프론트에서 소켓 연결이 확인 되면 
+//  console.log("somebody connnect") 
+io.on("connection", socket => {
+    console.log("somebody connnect")
+    sockets.push(socket.id);
+});
